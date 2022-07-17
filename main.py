7d71a6086394befe6c5d99fdf39f1761a86e7098
Saif Shahriar <saif.shahriar.pilotbd@gmail.com>
2022-07-17 23:05:24 +0600

    Error handled for usr_input.txt file.

--- main.py
+++ main.py
@@ -6,9 +6,15 @@ if os.path.exists("out.bf"):
     os.remove("out.bf")
 
 output = open("out.bf", "a")
+
+if not os.path.exists("usr_input.txt"):
+    input_file = open("usr_input.txt", "w")
+    input_file.write("Hello World!")
+
 input_file = open("usr_input.txt")
 usr_input = input_file.read()
 input_file.close()
+
 usr_input = list(usr_input)
 
 for char in usr_input:
